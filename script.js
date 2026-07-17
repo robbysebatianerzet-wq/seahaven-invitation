dda60e13d933395716ae2ccfdd5da8d083d65eab
robbysebatianerzet-wq <robbysebatianerzet@gmail.com>
2026-07-18 00:51:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===============================
-// Get Guest Name from URL
+// Guest Name from URL
 // ===============================
 
 const params = new URLSearchParams(window.location.search);
@@ -8,35 +8,66 @@ const guest = params.get("guest");
 
 const guestName = document.getElementById("guestName");
 
-if (guest) {
+if(guest){
 
-    guestName.textContent = decodeURIComponent(guest);
+    guestName.innerHTML = decodeURIComponent(guest);
 
-} else {
+}else{
 
-    guestName.textContent = "Our Distinguished Guest";
+    guestName.innerHTML = "Our Distinguished Guest";
 
 }
+
+
+
+// ===============================
+// AUTO FIT FONT
+// ===============================
+
+function autoFitText(){
+
+    let size = 42;
+
+    guestName.style.fontSize = size + "px";
+
+    while(
+        guestName.scrollHeight >
+        guestName.clientHeight &&
+        size > 18
+    ){
+
+        size--;
+
+        guestName.style.fontSize = size + "px";
+
+    }
+
+}
+
+autoFitText();
+
+
 
 // ===============================
 // RSVP Button
 // ===============================
 
-const rsvpButton = document.getElementById("rsvpButton");
+const rsvpButton =
+document.getElementById("rsvpButton");
 
-if (rsvpButton) {
+if(rsvpButton){
 
     const baseURL =
-        "https://docs.google.com/forms/d/e/1FAIpQLSfTNwyctNnz9KR4f4WHtUo234dq0oVlWAY77o9CVXWRkidDxg/viewform?usp=pp_url";
+"https://docs.google.com/forms/d/e/1FAIpQLSfTNwyctNnz9KR4f4WHtUo234dq0oVlWAY77o9CVXWRkidDxg/viewform?usp=pp_url";
 
-    if (guest) {
+    if(guest){
 
         rsvpButton.href =
             baseURL +
             "&entry.2095795296=" +
-            encodeURIComponent(decodeURIComponent(guest));
+            encodeURIComponent(guest);
 
-    } else {
+    }else{
 
         rsvpButton.href = baseURL;
 
