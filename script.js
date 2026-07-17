fa266d69abb44d1dc11d76df65d22a459c64471c
robbysebatianerzet-wq <robbysebatianerzet@gmail.com>
2026-07-17 22:55:19 +0800

    Update guest name positioning on invitation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// ===============================
+// Get Guest Name from URL
+// ===============================
+
 const params = new URLSearchParams(window.location.search);
 
 const guest = params.get("guest");
@@ -11,5 +15,21 @@ if (guest) {
 } else {
 
     guestName.innerHTML = "Our Distinguished Guest";
+
+}
+
+// ===============================
+// RSVP Button
+// ===============================
+
+const rsvpButton = document.getElementById("rsvpButton");
+
+if (rsvpButton && guest){
+
+    const formURL =
+        "https://docs.google.com/forms/d/e/1FAIpQLSdb1w9pBS1JxuLQS4lHLqbYqwonGQGFVrIus-An2ItS3EPKTg/viewform?usp=pp_url"
+        + "&entry.971898052=" + encodeURIComponent(guest);
+
+    rsvpButton.href = formURL;
 
 }
