4f3471e4cc69d1b02bbb247c44c37af3623b445c
robbysebatianerzet-wq <robbysebatianerzet@gmail.com>
2026-07-17 16:58:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Google Form RSVP
+function openRSVP() {
+    window.open(
+        "https://docs.google.com/forms/d/e/1FAIpQLSdb1w9pBS1JxuLQS4lHLqbYqwonGQGFVrIus-An2ItS3EPKTg/viewform",
+        "_blank"
+    );
+}
+
+// Download Invitation PDF
+function downloadInvitation() {
+    window.open(
+        "https://drive.google.com/file/d/ISI_FILE_ID_DISINI/view",
+        "_blank"
+    );
+}
